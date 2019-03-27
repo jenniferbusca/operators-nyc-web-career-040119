a7c6ddfcfc81a9c999e961adb77b0de135eec0d6
Jennifer Busca <jennifer.busca@gmail.com>
2019-03-27 15:47:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
 def unsafe?(speed)
-  speed > 60 ? true : false
+  if speed > 60 || speed < 40
+    true
+  else
+    false
 end
 
 
-
 def not_safe?(speed)
-	
+	speed > 60 ? true : false
 end
 	
 
